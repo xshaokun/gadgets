* implement cooling function calculation in astroeqs.py * create new function average(var) in fermi.py * correct unit in method get_dvolumn in fermi.py

astroeqs.py
@@ -12,6 +12,8 @@ Licensed under the MIT License, see LICENSE file for details
 import numpy as np
 from astropy import constants as cons
 from astropy import units as u
+from skpy.utilities.logger import fmLogger as mylog
+
 
 
 def Ledd(mbh):
@@ -76,10 +78,9 @@ def cs(gamma=5./3., mu=0.61, **kw):
         131.13606kms
     """
 
-    if 'T' in kw:
+    if ('T' in kw):
         T = kw['T']
         cs = np.sqrt(gamma* cons.k_B * T / (mu* cons.m_p))
-
     else:
         try:
             P = kw['P']
@@ -91,6 +92,7 @@ def cs(gamma=5./3., mu=0.61, **kw):
             print('Such error is unexpected, so explore the cause by yourself')
         
     return cs.to(u.km/u.s)
+
 
 def vkep(m, r=1.*u.kpc):
     """Keplerian velocity
@@ -104,6 +106,7 @@ def vkep(m, r=1.*u.kpc):
 
     vkep = cons.G * m / r
     return vkep.to(u.km/u.s)
+
 
 def eos(mu=0.61, **kw):
     """ Equation of State
@@ -126,12 +129,76 @@ def eos(mu=0.61, **kw):
     """
     
     coeff = (cons.k_B / mu / cons.m_p).cgs.value
-    if 'T' in kw and 'P' in kw:
+    if ('T' in kw and 'P' in kw):
         result = coeff * P * T
-    elif 'T' in kw and 'P' in kw:
+    elif ('T' in kw and 'P' in kw):
         result = kw['P'] / kw['T'] / coeff
-    elif 'P' in kw and 'rho' in kw:
+    elif ('P' in kw and 'rho' in kw):
         result = kw['P'] / kw['rho'] / coeff
     else:
         raise KeyError('Two values among T, rho and P shouled be given.')
     return result
+
+
+def radcool(temp, zmetal):
+    """ Cooling Function
+
+    This version redefines Lambda_sd
+    (rho/m_p)^2 Lambda(T,z) is the cooling in erg/cm^3 s
+
+    Args:
+        temp : temperature in the unit of K
+        zmetal: metallicity in the unit of solar metallicity
+
+    Return:
+        in the unit of erg*s*cm^3
+    """
+
+    tshape = temp.shape
+    tempflt = temp.flatten()
+    
+    qlog0 = np.zeros_like(tempflt)
+    qlog1 = np.zeros_like(tempflt)
+
+    for i, t in enumerate(tempflt):
+        tlog = np.log10(t)
+
+        # zero metal cooling coefficient Lambda_([Fe/H]=0
+        if tlog>=6.1:
+            qlog0[i] = -26.39 + 0.471*(np.log10(t + 3.1623e6))
+        elif tlog>=4.9:
+            arg = 10.**(-(tlog-4.9)/.5) + 0.077302
+            qlog0[i] = -22.16 + np.log10(arg)
+        elif tlog>=4.25:
+            bump1rhs = -21.98 - ((tlog-4.25)/0.55)
+            bump2lhs = -22.16 - ((tlog-4.9)/0.284)**2
+            qlog0[i] = max(bump1rhs,bump2lhs)
+        else:
+            qlog0[i] = -21.98 - ((tlog-4.25)/0.2)**2
+
+        if qlog0[i]==np.nan: mylog.warning('There is NaN.')
+        
+        # emission from metals alone at solar abundance
+        if tlog>=5.65:
+            tlogc = 5.65
+            qlogc = -21.566
+            qloginfty = -23.1
+            p = 0.8
+            qlog1[i] = qlogc -p*(tlog - tlogc)
+            qlog1[i] = max(qlog1[i],qloginfty)
+        else:         
+            tlogm = 5.1
+            qlogm = -20.85
+            sig = 0.65
+            qlog1[i] = qlogm - ((tlog - tlogm)/sig)**2
+
+    qlambda0 = 10.**qlog0
+
+    qlambda1 = 10.**qlog1
+
+    # final cooling coefficient Lambda_sd:
+    radcoolsd = qlambda0 + zmetal.flatten()*qlambda1
+    radcoolsd = radcoolsd.reshape(tshape)
+    
+
+    return radcoolsd

fermi.py
@@ -9,17 +9,20 @@ Licensed under the MIT License, see LICENSE file for details
 """
 
 
+import os
+import sys
 import numpy as np
 import pandas as pd
 from astropy import units as u
 from astropy import constants as cons
-import os
-import sys
+from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 from skpy.utilities.logger import fmLogger as mylog
 
 PARA = "Parameter: {0:<20}\t = {1:<10}"
 PROP = "Property: {0:<20}\t = {1:<10}"
 DIME = "Dimension: {0:<20}\t : {1:<10}"
+
 
 class FermiData(object):
     """Tools for reading output data of fermi.f
@@ -41,7 +44,7 @@ class FermiData(object):
             the numver of logarithmic spaced grids.
         ezone: float
             the length of even spaced region.
-        zone: int
+        izone: int
             the number of total grids in one direction. (both directions are same.)
         reso: float
             the resolution for the even spaced grids.
@@ -63,7 +66,7 @@ class FermiData(object):
             self.iezone = int(dims[0])
             self.ilzone = int(dims[1])
             self.ezone = float(dims[2])
-            self.zone = self.iezone + self.ilzone
+            self.izone = self.iezone + self.ilzone
             self.reso = self.ezone/self.iezone
             self.kprint = self.inp[20].split()[:-1]
 
@@ -96,7 +99,6 @@ class FermiData(object):
         para = line[col]
 
         return para
-
 
 
     def read_coord(self, var):
@@ -135,7 +137,7 @@ class FermiData(object):
                 in the unit of kpc^-3
         """
 
-        coord = self.read_coord('x')
+        coord = self.read_coord('x') * u.kpc.to(u.cm)
         rr, zz = np.meshgrid(coord, coord)
         dz = np.diff(zz, axis=0)[:,:-1]
         dr2 = np.diff(rr*rr)[:-1]
@@ -157,8 +159,28 @@ class FermiData(object):
         Rh, zh = np.meshgrid(coord, coord)
         theta = np.arctan(Rh/zh)*u.rad.to(u.deg)
         return theta
-        
 
+
+    def get_radius(self, var):
+        """get distance from origion
+
+        In the cylindrical symmetric coordinate, this distance from the origin r^2 = R^2 + z^2
+
+        Args:
+            var: str
+                grid boundary 'x' or grid center 'xh'.
+        Return:
+            rh: numpy.ndarray
+                in the unit of kpc
+        """
+
+        coord = self.read_coord(var)
+        Rh, zh = np.meshgrid(coord, coord)
+        rh = np.sqrt(Rh*Rh+zh*zh)
+
+        return rh
+
+        
     def read_var(self, var, kprint):
         """read '*ascii.out*' variable outputs.
 
@@ -195,7 +217,7 @@ class FermiData(object):
         data = np.fromfile(file,dtype=float,sep=" ")
         dmax = data.max()
         dmin = data.min()
-        data = data.reshape([self.zone,self.zone])
+        data = data.reshape([self.izone,self.izone])
         data = data.T  # reverse index from fortran
         mylog.info(PROP.format("(min, max)",f"({dmin}, {dmax})"))
 
@@ -251,6 +273,7 @@ def meshgrid(coord,rrange,zrange):
     mylog.info(DIME.format('xh', f'z-{R.shape[0]} R-{R.shape[1]}'))
 
     return R, z
+
 
 def mesh_var(data, var, meshgrid):
     """construct meshgrid based on variable data.
@@ -336,7 +359,7 @@ def slice_mesh(data, coord, direction='z', kpc=0):
         raise ValueError("Only 'z' and 'r' are allowed.")
 
 
-def count_enclosed(coord, den, weight='mass', direction='r', interval=5):
+def count_enclosed(coord, den, weight='mass', direction='r', interval=1):
     """ sum up the grids inside out.
 
     given a direction, sum up mass or volume of the grids inside out and output a profile.
@@ -363,6 +386,7 @@ def count_enclosed(coord, den, weight='mass', direction='r', interval=5):
         >>> summass = count_enclosed(x, den4)
     """
 
+    coord = coord*u.kpc.to(u.cm)
     rr, zz = np.meshgrid(coord, coord)
     dz = np.diff(zz, axis=0)[:,:-1]
     dr2 = np.diff(rr*rr)[:-1]
@@ -383,15 +407,51 @@ def count_enclosed(coord, den, weight='mass', direction='r', interval=5):
         
     sumup = [mcell[dirtn[1::,1::]<=loc].sum() for loc in coord[1::interval]]
 
-    return sumup
+    return np.ndarray(sumup)
+
+    
+def average(coord, var, interval=1, weights=None):
+    """average the properties of the cells within the same spherical shell
 
     
 
+    """
+    
+    Rh, zh = np.meshgrid(coord, coord)
+    rh = np.sqrt(Rh*Rh+zh*zh)
+
+    if not rh.shape == var.shape:
+        mylog.error(f"Coordinate should be grid-centric.")
+
+    if weights is not None and weights.shape != var.shape:
+        mylog.error(f"The shape of weights {weights.shape} do not match the shape of variables {var.shape}.")
+
+    rhflt = rh.flatten()
+    varflt = var.flatten()
+
+    bins = np.arange(0, coord.max(), interval)
+    indic = np.digitize(rhflt,bins)
+    avevar = np.zeros_like(bins)
+
+    for i in np.arange(1,bins.size+1):
+        varmsk = varflt[indic==i]
+        if weights is not None:
+            wflt = weights.flatten()
+            wts = wflt[indic==i]
+            avevar[i-1] = np.average(varmsk, weights=wts)
+        else:
+            avevar[i-1] = np.average(varmsk)
+        
+    return bins, avevar
 
 
 
-# class Image(object):
-    # continue
+# class FermiImage(Figure):
+    # """ Plot 1D profile
+# 
+    # """
+    # def __init__(self, figure_size, fontsize):
+        # super(FermiImage, self).__init__(figsize=figure_size,)
 
 
 def find_nearst(arr,target):
@@ -414,7 +474,3 @@ def latex_float(f):
         return r"{0} \times 10^{{{1}}}".format(base, int(exponent))
     else:
         return float_str
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
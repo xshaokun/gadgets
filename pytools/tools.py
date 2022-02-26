c86e02afcd667f1f044da9ed49f800009730806d
xshaokun <xshaokun@foxmail.com>
2022-02-26 21:12:47 +0800

    add new func str_to_number()

--- pytools/tools.py
+++ pytools/tools.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 """tools.py - some helpful functions.
 
-Last Modified: 2021.05.24
-
-Copyright(C) 2021 Shaokun Xie <https://xshaokun.com>
 Licensed under the MIT License, see LICENSE file for details
 """
 
-import numpy as np
+import re
 
+import numpy as np
 
 def nearest(arr,target):
     """get the index of nearest value
@@ -24,3 +22,20 @@ def nearest(arr,target):
     index = np.abs(arr-target).argmin()
     
     return index
+
+def str_to_number(string):
+    """turn a string of number into correct type"""
+    # This is referred to
+    # https://stackoverflow.com/questions/41668588/regex-to-match-scientific-notation
+    _FLOAT_PATTERN_ = r"^[+-]?(?:0|[1-9]\d*)(?:\.\d*)$"
+    _SCI_NOT_PATTERN_ = r"^[+-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+-]?\d+)$"
+    _FLOAT_REGEXP_ = rf"{_FLOAT_PATTERN_}|{_SCI_NOT_PATTERN_}"
+    _INT_REGEXP_ = r"^[+-]?\d+$"
+    float_match = re.fullmatch(_FLOAT_REGEXP_, string)
+    int_match = re.fullmatch(_INT_REGEXP_, string)
+    if float_match:
+        return float(string)
+    elif int_match:
+        return int(string)
+    else:
+        raise ValueError(f"Cannot identify the string as a number: {string}")
